c4af7dddb107fd854a5e78c54322dc59245c46ac
Hans Ulrich Niedermann <hun@n-dimensional.de>
2002-08-19 02:38:16 +0000

    remove files not in CVS, echo commands before execution
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/gphoto2-manual@5041 67ed7778-7388-44ab-90cf-0a291f65f57c

--- autogen.sh
+++ autogen.sh
@@ -2,8 +2,23 @@
 
 here=$(dirname $0)
 
+echo "#### Cleaning directory tree from non-CVS files"
+for dir in $(find . -type d)
+do
+(
+    if cd "${dir}" && test -s .cvsignore
+    then
+	echo "Cleaning directory from non-CVS files: ${dir}"
+	rm -rf $(cat .cvsignore)
+    fi
+)
+done
+echo "Finished cleaning."
+
+cat<<EOF | while read command; do echo "#### Executing \"$command\"..."; $command; done
 aclocal -I m4
 automake --gnu --add-missing
 autoconf
+EOF
 
-echo "You may run $here/configure now."
+echo "#### You may run $here/configure now."
