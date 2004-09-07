644406a31d5d59b61a0cb48e10b3a0a85534c51b
Hans Ulrich Niedermann <hun@n-dimensional.de>
2004-09-07 19:52:09 +0000

    add sf.net webserver settings, but keys/accounts/password/etc. still remain secret :)
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/gphoto2-manual@7332 67ed7778-7388-44ab-90cf-0a291f65f57c

---
+++ webserver-settings.mk
@@ -0,0 +1,8 @@
+# "ssh $(SERVER)" must work
+SERVER = sf
+
+# where and how to do things on the web server
+TARGET_DIR = /home/groups/g/gp/gphoto/htdocs/doc/manual/
+FMODE = 0664
+DMODE = 0775
+GROUP = gphoto
